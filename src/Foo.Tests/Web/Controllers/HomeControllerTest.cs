e1f728bb3ac0425f2a7c1daf8234a2bf8bd921e1
Troels Thomsen <tt@appharbor.com>
2011-02-02 21:07:49 -0800

    Initialize new home controller instance for test class

--- src/Foo.Tests/Web/Controllers/HomeControllerTest.cs
+++ src/Foo.Tests/Web/Controllers/HomeControllerTest.cs
@@ -1,6 +1,14 @@
-﻿namespace Foo.Tests.Web.Controllers
+﻿using Foo.Web.Controllers;
+
+namespace Foo.Tests.Web.Controllers
 {
 	public class HomeControllerTest
 	{
+		private readonly HomeController _controller;
+
+		public HomeControllerTest()
+		{
+			_controller = new HomeController();
+		}
 	}
 }
